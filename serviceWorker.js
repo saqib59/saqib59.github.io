63eadf40a41ee3901cf76fabd4f94cc88874d5b7
Saqib <saqibali80400@gmail.com>
2023-05-25 12:05:12 +0500

    service worker updated for assets

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,19 +1,38 @@
 const staticDevCoffee = "dev-coffee-site-v1";
-const assets = [
-  "/pwa-with-vanilla-js",
-  "/pwa-with-vanilla-js/index.html",
-  "/pwa-with-vanilla-js/css/style.css",
-  "/pwa-with-vanilla-js/js/app.js",
-  "/pwa-with-vanilla-js/images/coffee1.jpg",
-  "/pwa-with-vanilla-js/images/coffee2.jpg",
-  "/pwa-with-vanilla-js/images/coffee3.jpg",
-  "/pwa-with-vanilla-js/images/coffee4.jpg",
-  "/pwa-with-vanilla-js/images/coffee5.jpg",
-  "/pwa-with-vanilla-js/images/coffee6.jpg",
-  "/pwa-with-vanilla-js/images/coffee7.jpg",
-  "/pwa-with-vanilla-js/images/coffee8.jpg",
-  "/pwa-with-vanilla-js/images/coffee9.jpg"
-];
+if (window.location.hostname === 'localhost') {
+    const assets = [
+    "/pwa-with-vanilla-js",
+    "/pwa-with-vanilla-js/index.html",
+    "/pwa-with-vanilla-js/css/style.css",
+    "/pwa-with-vanilla-js/js/app.js",
+    "/pwa-with-vanilla-js/images/coffee1.jpg",
+    "/pwa-with-vanilla-js/images/coffee2.jpg",
+    "/pwa-with-vanilla-js/images/coffee3.jpg",
+    "/pwa-with-vanilla-js/images/coffee4.jpg",
+    "/pwa-with-vanilla-js/images/coffee5.jpg",
+    "/pwa-with-vanilla-js/images/coffee6.jpg",
+    "/pwa-with-vanilla-js/images/coffee7.jpg",
+    "/pwa-with-vanilla-js/images/coffee8.jpg",
+    "/pwa-with-vanilla-js/images/coffee9.jpg"
+  ];
+
+} else {
+     const assets = [
+    "/",
+    "/index.html",
+    "/css/style.css",
+    "/js/app.js",
+    "/images/coffee1.jpg",
+    "/images/coffee2.jpg",
+    "/images/coffee3.jpg",
+    "/images/coffee4.jpg",
+    "/images/coffee5.jpg",
+    "/images/coffee6.jpg",
+    "/images/coffee7.jpg",
+    "/images/coffee8.jpg",
+    "/images/coffee9.jpg"
+  ];
+}
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
